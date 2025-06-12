0572e2e5de0233d03ad4d6f2ae0e5f9d1ee15f1d
tumelo-maja <mogale.ramsey@gmail.com>
2025-06-12 21:27:59 +0100

    Add cloudinary configs

--- a_core/settings.py
+++ a_core/settings.py
@@ -18,6 +18,16 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+cloudinary.config( 
+  	cloud_name = os.environ.get("CLOUDINARY_NAME"),
+  	api_key = os.environ.get("CLOUDINARY_API_KEY"),
+  	api_secret = os.environ.get("CLOUDINARY_SECRET_KEY")
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
