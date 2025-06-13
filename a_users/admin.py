f749fc1f94f2b6b9de8dbc25b1adc911707ca19a
tumelo-maja <mogale.ramsey@gmail.com>
2025-06-13 17:35:01 +0100

    Add custom chats and deployment notes

--- a_users/admin.py
+++ a_users/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'displayname', 'id')
+
+admin.site.register(Profile, ProfileAdmin)
